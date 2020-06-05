ad86b0858d96e2120911f0582da7adb33ca9df62
Alexander Shatravka <shatravgo2@gmail.com>
2020-06-06 02:06:01 +0300

    upload landing

---
+++ js/functions.js
@@ -0,0 +1,205 @@
+$(document).ready(function() {
+  initIntroSlider();
+  initFormCustomFocus();
+  initAnchorsScroll();
+  initServicesSlider();
+  initPartnersSlider();
+  initTabs();
+  initFixedHeader();
+  initMobileMenu();
+  initFormValidation();
+  initRemoveErr();
+  initTelegramForms();
+});
+
+function initIntroSlider() {
+  var slickElement = $('.intro-slider');
+
+  slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+    var statusCurrent = $('.slider-progress .current');
+    var statusLast = $('.slider-progress .last');
+    var i = (currentSlide ? currentSlide : 0) + 1;
+    statusCurrent.text(''+i);
+    statusLast.text(''+slick.slideCount);
+  })
+
+  slickElement.slick({
+    infinite: true,
+    dots: false,
+    prevArrow: $('.intro-slider-prev'),
+    nextArrow: $('.intro-slider-next')
+  })
+
+  jQuery(window).on('resize load', function() {
+    var viewportWidth = jQuery(window).width();
+
+    if (viewportWidth < 768) {
+      $('.intro-slider').slick('unslick');
+    } else {
+      // Do some thing
+    }
+  });
+}
+
+function initFixedHeader() {
+  var fixedItem = jQuery("header"),
+    win = jQuery(window);
+  win.on('load resize scroll', function (e) {
+    var winTop = win.scrollTop();
+    if ($(window).width() > 0) {
+      if (winTop && winTop > 50) {
+        fixedItem.addClass("fixed");
+      } else {
+        fixedItem.removeClass("fixed");
+      }
+      pointRemember = winTop;
+    }
+    // else fixedItem.addClass("slideUp");
+  });
+}
+
+function initMobileMenu() {
+  $('.burger-btn').on('click', function () {
+    $(this).toggleClass('active');
+    $('.main-nav').toggleClass('mobile-show');
+    $('.header').toggleClass('mobile-menu-enable');
+  })
+}
+
+function initFormCustomFocus() {
+  $('.custom-input').on('blur', function () {
+    $('.input-wrapper').removeClass('focused');
+    if ($(this).val()) {
+      $(this).parents('.input-wrapper').find('.placeholder').addClass('hide');
+    } else {
+      $(this).parents('.input-wrapper').find('.placeholder').removeClass('hide');
+    }
+  });
+  $('.custom-input').on('focus', function () {
+    $(this).parents('.input-wrapper').addClass('focused');
+  })
+}
+
+function initAnchorsScroll() {
+  $(document).on('click', 'a.anchor-link[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('.main-nav').removeClass('mobile-show');
+
+    $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+  });
+}
+
+function initServicesSlider() {
+  $('.services-slider').slick({
+    dots: false,
+    nextArrow: $('.services-arrows .slider-next'),
+    prevArrow: $('.services-arrows .slider-prev'),
+  })
+}
+
+function initTabs() {
+  $('.row-tab-links').on('click', 'a', function (e) {
+    e.preventDefault();
+    $('.row-tab-links>a').removeClass('active');
+    $(this).addClass('active');
+    $('.tabs-content .tab').removeClass('show');
+    $('.tabs-content .tab').eq($(this).index()).addClass('show');
+  })
+}
+
+function initPartnersSlider() {
+  var slickElement = $('.partners-slider');
+
+  slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+    var statusCurrent = $('.partners-slider-progress .current');
+    var statusLast = $('.partners-slider-progress .last');
+    var i = (currentSlide ? currentSlide : 0) + 1;
+    statusCurrent.text(''+i);
+    statusLast.text(''+slick.slideCount);
+  })
+
+  slickElement.slick({
+    infinite: true,
+    dots: false,
+    prevArrow: $('.partners-slider-prev'),
+    nextArrow: $('.partners-slider-next'),
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+    ],
+  })
+}
+
+function initTelegramForms() {
+  jQuery('#main-form-submit').on('click', function(e) {
+    e.preventDefault();
+    initFormValidation();
+    var errors = jQuery('#form-main .has-error');
+    if (errors.length) {
+      return false;
+    }
+    var form_data = jQuery('#form-main').serialize();
+    jQuery.ajax({ //telegram to admins
+      type: "POST",
+      url: "telegram.php",
+      data: form_data,
+      success: function() {
+        // jQuery('.thank-massage').addClass('done');
+        location = '/thank-you.html';
+        setTimeout(function() { jQuery('.thank-massage').removeClass('done'); }, 3000);
+      },
+    });
+    return false;
+  })
+}
+
+function initFormValidation() {
+  jQuery('#main-form-submit').on('click', function(e) {
+    e.preventDefault();
+    var name = jQuery(this).parents('form').find('input[name="name"]');
+    var tel = jQuery(this).parents('form').find('input[name="phone"]');
+    // var email = jQuery(this).parents('form').find('input[name=email]');
+
+    if (name.val()=='') {
+      name.addClass('has-error');
+    } else name.removeClass('has-error');
+
+    if (tel.val()=='') {
+      tel.addClass('has-error');
+    } else tel.removeClass('has-error');
+
+    // if (email.val()=='') {
+    //     email.addClass('error');
+    // } else email.removeClass('error');
+  });
+}
+
+function initRemoveErr(){
+  jQuery('input[name=name],input[name=email],input[name=phone]').focusout(function(){
+    var name = jQuery(this).parents('form').find('input[name=name]');
+    var tel = jQuery(this).parents('form').find('input[name=phone]');
+    // var email = jQuery(this).parents('form').find('input[name=email]');
+
+    if (name.val()=='') {
+      name.addClass('has-error');
+    } else name.removeClass('has-error');
+
+    if (tel.val()=='') {
+      tel.addClass('has-error');
+    } else tel.removeClass('has-error');
+
+    // if (email.val()=='') {
+    //   email.addClass('has-error');
+    // } else email.removeClass('has-error');
+  });
+}
---
+++ telegram.php
@@ -0,0 +1,27 @@
+<?php
+    // $service = $_POST['service'] ? $_POST['service'] : 'не указано';
+    $name = $_POST['name'];
+    $phone = $_POST['phone'];
+    // $email = $_POST['email'];
+    $message = $_POST['message'];
+    // $file = $_POST['file'];
+
+    $token = "1245666893:AAG0dKYlA3ekDdCfwt7W4EETe4Kca0Akmzk";
+    $chat_id = "-473060733";
+
+    $arr = array(
+        'услуга: ' => $service,
+        'имя: ' => $name,
+        'телефон: ' => $phone,
+        // 'почта: ' => $email,
+        //'сообщение: ' => $message,
+        // 'файл: ' => $file
+    );
+
+    foreach($arr as $key => $value) {
+        $txt .= "<b>".$key."</b> ".$value."%0A";
+    };
+
+    //var_dump($txt);
+    $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+?>
